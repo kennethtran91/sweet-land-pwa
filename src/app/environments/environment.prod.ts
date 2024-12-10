167a933884c19190213bde52e69d8c365066f698
kennethtran91 <kenneth.tran@jbhunt.com>
2024-12-10 17:31:10 -0600

    fix shit

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-    production: true,
-    firebaseConfig: {
-      apiKey: "YOUR_API_KEY",
-      authDomain: "YOUR_AUTH_DOMAIN",
-      projectId: "YOUR_PROJECT_ID",
-      storageBucket: "YOUR_STORAGE_BUCKET",
-      messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-      appId: "YOUR_APP_ID"
-    },
-    apiUrl: '',
-    ADMIN_PASSWORD: ''
-  };
+  production: true,
+  firebaseConfig: {
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    appId: 'YOUR_APP_ID',
+  },
+  apiUrl: process.env.apiUrl,
+  ADMIN_PASSWORD: process.env.DATABASE_URL,
+};
